44a74ad2fc651aa13c560cba3a688cd30c78f91f
Sophie <sophie45656@gmail.com>
2024-07-05 10:15:37 +0800

    20240705

--- index.js
+++ index.js
@@ -4,25 +4,26 @@ const { singers } = require("./singer.json");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("這是首頁");
+//   res.send("這是首頁");
+    res.redirect("/singer/3.html");
 });
 
 app.get("/singer/:id.html", (req, res) => {
-  const id = parseInt(req.params.id);
-  const result = singers.find(singer => {
-    if (singer.id === id) {
-      return true;
+    const id = parseInt(req.params.id);
+    const result = singers.find(singer => {
+        if(singer.id === id){
+            return true;
+        }
+    });
+    if(!result){
+        // 沒有結果
+        // res.statusCode = 404; // http 模組中就有的
+        res.status(404);
+        res.send("<h1>404 - 找不到歌手</h1>");
+        return;
     }
-  });
-  if (!result) {
-    // 沒有結果
-    // res.statusCode = 404; // http 模組中就有的
-    res.status(404);
-    res.send("<h1>404 - 找不到歌手</h1>");
-    return;
-  }
-  // 有結果
-  res.send(`<!DOCTYPE html>
+    // 有結果
+    res.send(`<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
